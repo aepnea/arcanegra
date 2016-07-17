5f7560c533819898e5a88f5793a087f217800a7d
Cristian Vásquez Parga <cvasquez@zaetachile.cl>
2016-07-17 16:45:02 -0400

    seed states

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+State.create([{name: 'Región de Arica y Parinacota'}, \
+			   {name: 'Región de Tarapacá'}, \
+			   {name: 'Región de Antofagasta'}, \
+			   {name: 'Región de Atacama'}, \
+			   {name: 'Región de Coquimbo'}, \
+			   {name: 'Región de Valparaíso'}, \
+			   {name: 'Región Metropolitana'}, \
+			   {name: 'Región del Libertador General Bernardo O\'Higgins'}, \
+			   {name: 'Región del Maule'}, \
+			   {name: 'Región del Biobío'}, \
+			   {name: 'Región de La Araucanía'}, \
+			   {name: 'Región de Los Ríos'}, \
+			   {name: 'Región de Los Lagos'}, \
+			   {name: 'Región de Aysén'}, \
+			   {name: 'Región de Magallanes'}
+			   ])
