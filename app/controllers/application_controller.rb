22f6097a717e5367e40f651d1666bc3dffa382d4
Cristian Vásquez Parga <cvasquez@zaetachile.cl>
2016-09-07 23:53:53 -0300

    probando current_customer.id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :set_shopping_cart 
+  before_action :set_shopping_cart
   before_filter :store_current_location, :unless => :devise_controller?
 
 
@@ -11,21 +11,21 @@ private
 	def store_current_location
 		store_location_for(:user, request.url)
 	end
-	
+
   def after_sign_out_path_for(resource)
    	request.referrer || root_path
  	end
-  
- 
+
+
 
   	def set_shopping_cart
   		### preguntando si el usuario tiene o no creado un carro
   		if cookies[:cart_id].blank?
-  		#si no tiene se le crea uno		
-  			@shopping_cart = Cart.create!(ip: request.remote_ip)
+  		#si no tiene se le crea uno
+  			@shopping_cart = Cart.create!(customer_id: current_customer.id, ip: request.remote_ip)
   			cookies[:cart_id] = {:value => @shopping_cart.id, :expires => 24.hour.from_now}
   		else
-  		# si ya tiene uno se busca y se mete  a variable de clase	
+  		# si ya tiene uno se busca y se mete  a variable de clase
   			@shopping_cart = Cart.find(cookies[:cart_id])
   		end
   		## si por algun motivo no se encuentra el carro se le crea uno nuevo
@@ -35,4 +35,3 @@ private
   			cookies[:cart_id] = {:value => @shopping_cart.id, :expires => 24.hour.from_now}
   	end
 end
-
